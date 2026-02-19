69fd033cf74a67cf68a07a0f538f9f1bc485d9c8
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-02-19 09:41:28 -0500

    basic server config complete

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	// Create a new mux and server struct
+	filepathRoot := "."
+	port := ":8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
 
+	// Start the server
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }

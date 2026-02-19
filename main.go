e13e7cd11ab2336e17bce1dd046117e6f5531423
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-02-19 11:37:59 -0500

    metrics and reset endpoints work

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitsCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) hitsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	// Create a new mux and server struct
 	filepathRoot := "."
 	port := ":8080"
+	healthHandler := http.HandlerFunc(healthCheck)
+	c := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", c.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", c.hitsCount)
+	mux.HandleFunc("/reset", c.hitsReset)
 	server := &http.Server{
 		Addr:    port,
 		Handler: mux,
